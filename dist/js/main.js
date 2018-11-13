9904ece0c7254a0de7f5670a7fbee81eb5cb47e5
Ruslan Temirkhanov <ruslan@Ruslans-MBP.fios-router.home>
2018-11-13 01:18:00 -0500

    New simplified version

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,32 +1,28 @@
 // Select Dom Items
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
-const menuBranding = document.querySelector('.menu-branding');
-const menuNav = document.querySelector('.menu-nav');
-const navItems = document.querySelectorAll('.nav-item');
-const homeText = document.querySelectorAll('.home-text')
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".nav-item");
+const homeText = document.querySelectorAll(".home-text");
 // Set initial state of menu
 let showMenu = false;
 
-menuBtn.addEventListener('click', toggleMenu);
+menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-  if(!showMenu) {
-    homeText.forEach(i => i.classList.add('hidden'));
-    menuBtn.classList.add('close');
-    menu.classList.add('show');
-    menuNav.classList.add('show');
-    menuBranding.classList.add('show');
-    navItems.forEach(item => item.classList.add('show'));
+  if (!showMenu) {
+    homeText.forEach(i => i.classList.add("hidden"));
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
     showMenu = true;
-  }
-  else {
-    homeText.forEach(i => i.classList.remove('hidden'));
-    menuBtn.classList.remove('close');
-    menu.classList.remove('show');
-    menuNav.classList.remove('show');
-    menuBranding.classList.remove('show');
-    navItems.forEach(item => item.classList.remove('show'));
+  } else {
+    homeText.forEach(i => i.classList.remove("hidden"));
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
     showMenu = false;
   }
 }
